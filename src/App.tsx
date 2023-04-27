7e5ea56af01df8d1d04a49750a601ecb40927a0b
larissa.rangel <larissa.rangel@MacBook-Air.local>
2023-04-27 17:52:04 -0300

    enable analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import { 
   BrowserRouter as Router, Route, Routes 
 } from 'react-router-dom'
+import
+ { Analytics } 
+from
+ 
+'@vercel/analytics/react'
+;
 
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home/Home'
@@ -28,6 +34,7 @@ function App() {
         </Routes>
 
       </Router>
+      <Analytics />
       
     </div>
   )
