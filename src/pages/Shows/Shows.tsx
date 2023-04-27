0322c990802679172fa33fab349b3dde83dac19d
larissa.rangel <larissa.rangel@MacBook-Air.local>
2023-04-27 18:09:03 -0300

    remove broken files

--- src/pages/Shows/Shows.tsx
+++ src/pages/Shows/Shows.tsx
@@ -1,13 +1,9 @@
-import ShowsContainer from "../../components/showsContainer/ShowsContainer";
-
-
 const Shows = () => {
     return (
       <div className="shows">
         <div className="shows__header">
           <h1>Exposições</h1>
         </div>
-        <ShowsContainer />
       </div>
     );
 };
