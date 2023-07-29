87ef5d9c3ef569f221468649d015531d3789304f
larissa.rangel <larissa.rangel@MacBook-Air.local>
2023-07-29 11:17:41 -0300

    connect home to databank images

--- src/components/carousel/homeCarousel/HomeCarousel.tsx
+++ src/components/carousel/homeCarousel/HomeCarousel.tsx
@@ -1,46 +1,60 @@
-import { useState } from "react";
-import DotNavigation from "../dotsNavigation/DotNavigation";
 import './homeCarousel.scss';
 
-export interface ImageProps {
-    url: string;
-}
+import { useEffect, useState } from "react";
 
-const images: ImageProps[] = [
-    { url: 'https://www.portasvilaseca.com.br/wp/wp-content/uploads/2021/04/KC_111_web.jpg' },
-    { url: 'https://projetoafro.com/wp-content/uploads/2020/07/projeto_afro_mapeamento_artista_kika_carvalho_8.jpeg' },
-    { url: 'https://static.aosfatos.org/media/images/arts/Aos-Fatos-_-Leilao-_-4.jpg' },
-    { url: 'https://www.portasvilaseca.com.br/wp/wp-content/uploads/2021/04/Emi_fe_e_2._Oleo_e_acrilica_sobre_tela._150_x_100_cm.jpg' }
-]
+import DotNavigation from "../dotsNavigation/DotNavigation";
+
+export interface Image {
+    id: number;
+    imageURL: string;
+    legenda?: string;
+    conteudo?: string;
+};
 
 const HomeCarousel = () => {
 
     const [currentImage, setCurrentImage] = useState(0);
+    const [data, setData] = useState<Image[]>([]);
 
+    useEffect(() => {
+        async function fetchData() {
+          const response = await fetch('https://kika-api.vercel.app/api/imagem');
+          const jsonData = await response.json();
+          setData(jsonData);
+        }
+    
+        fetchData();
+    }, []);
+      
+    
     const handleDotClick = (index: number) => {
         setCurrentImage(index);
     };
-
+    
     setTimeout(() => {
-        if(currentImage < images.length - 1 ){
+        if(currentImage < data.length - 1 ){
             setCurrentImage(currentImage + 1);
         } else {
             setCurrentImage(0);
         }
     }, 3000)
 
+    if(!data){
+      return null;
+    };
+
     return (
         <div id="carousel" className="homeCarousel">
-            {images.map((image, index) => (
+            {data.map((data, index) => (
                 <img
                 key={index}
-                src={image.url}
+                src={data.imageURL}
                 alt=""
                 style={{ display: index === currentImage ? 'block' : 'none' }}
                 />
             ))}
             <div className="homeCarousel__buttons">
-                <DotNavigation numDots={images.length} activeIndex={currentImage} onDotClick={handleDotClick} />
+                <DotNavigation numDots={data.length} activeIndex={currentImage} onDotClick={handleDotClick} />
             </div>
         </div>
     );
