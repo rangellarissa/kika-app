9beee63394cf0a2259975ce781d2331e44720dfa
Larissa Rangel <72013882+rangellarissa@users.noreply.github.com>
2026-05-09 12:33:38 -0300

    adjust show detail page

--- src/pages/Shows/ShowDetails/ShowDetails.tsx
+++ src/pages/Shows/ShowDetails/ShowDetails.tsx
@@ -32,18 +32,18 @@ const ShowDetails = () => {
       <div className="show-details__header">
         <h1>{show.titulo}</h1>
       </div>
-        <div className="show-details__content" key={show.id}>
-          <div className="show-details__content--text">
-            <p>{show.ano}</p>
-            <p>{show.local}</p>
-            <FormattedText text={show.texto} />
-          </div>
-          {show.imagem && (
-            <div className="show-details__content--image">
-              <img src={show.imagem.imageURL} alt={show.titulo} />
-            </div>
-          )}
+      <div className="show-details__content" key={show.id}>
+        <div className="show-details__content--text">
+          <p>{show.ano}</p>
+          <p>{show.local}</p>
+          <FormattedText text={show.texto} />
         </div>
+        {show.imagem && (
+          <div className="show-details__content--image">
+            <img src={show.imagem.imageURL} alt={show.titulo} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
